98c65356518bb5f0be8684a5d2689e55bc74e82a
Ashley Flynn <ashnflynn@gmail.com>
2026-05-09 10:21:40 -0500

    assignment finished, but want to fix the sort function so that it sorts by card value and not by the code

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -3,19 +3,20 @@ import "./Assignment.css";
 
 export default function Assignment() {
   const [deck, setDeck] = useState("");
-    const [cards, setCards] = useState([]);
-    const [filteredCards, setFilteredCards] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [filteredCards, setFilteredCards] = useState([]);
+  const [suit, setSuit] = useState("");
 
-
-    async function getDeck() {
-      const res = await fetch(
-        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1",
-      );
-      const data = await res.json();
-        setDeck(data.deck_id);
-        setCards([]);
-      console.log(data.deck_id);
-    }
+  async function getDeck() {
+    const res = await fetch(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1",
+    );
+    const data = await res.json();
+    setDeck(data.deck_id);
+    setCards([]);
+    setFilteredCards([]);
+    console.log(data.deck_id);
+  }
 
   useEffect(() => {
     try {
@@ -39,36 +40,61 @@ export default function Assignment() {
     } catch (error) {
       console.log(error);
     }
+  }
+
+    function chooseSuit(e) {
+        const selectedSuit = e.target.value;
+        const filteredSuit = cards.filter((card) => card.suit === selectedSuit);
+        setFilteredCards(filteredSuit)
+  }
+
+  function filterRedCards() {
+    const filteredRed = cards.filter((card) =>
+      ["HEARTS", "DIAMONDS"].includes(card.suit),
+    );
+    setFilteredCards(filteredRed);
+  }
+
+  function filterBlackCards() {
+    const filteredBlack = cards.filter((card) =>
+      ["SPADES", "CLUBS"].includes(card.suit),
+    );
+    setFilteredCards(filteredBlack);
+  }
+
+    // unlike filter which returns a new array, sort method mutates the array, so spread operator is used
+    // this is not actually sorting by card value since card.code is a mix of letters and numbers.
+
+  function sortValue() {
+    const sortedCards = [...cards].sort((a, b) => {
+      return a.code.localeCompare(b.code);
+    });
+    setFilteredCards(sortedCards);
     }
     
-    function filterRedCards() {
-        const filteredCards = cards.sort()
-    }
+    // create a new sort function to try to transform the face cards to numbers and give each card a value that can be sorted numerically
 
-        function filterBlackCards() {
-          const filteredCards = cards.sort();
-    }
-    
-        function sortCards() {
-          const filteredCards = cards.sort();
-        }
+    // function sortValue() {
+        
+    // }
 
-    const displayCards = filteredCards.length > 0 ? filteredCards : cards
+  const displayCards = filteredCards.length > 0 ? filteredCards : cards;
 
   return (
     <div>
       <div className="app-container">
         <header className="controls">
-          <select>
+          <select onChange={chooseSuit}>
             <option>Choose a Suit</option>
             <option value="HEARTS">Hearts</option>
             <option value="DIAMONDS">Diamonds</option>
             <option value="CLUBS">Clubs</option>
             <option value="SPADES">Spades</option>
           </select>
-          <button>Sort by Value</button>
-          <button>Show Only Red</button>
-          <button>Show Only Black</button>
+
+          <button onClick={sortValue}>Sort by Value</button>
+          <button onClick={filterRedCards}>Show Only Red</button>
+          <button onClick={filterBlackCards}>Show Only Black</button>
           <button onClick={() => getDeck()}>New Deck</button>
         </header>
 
